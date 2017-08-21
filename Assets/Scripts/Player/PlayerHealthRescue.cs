6c1478a31a2994c37ee40244c7ebd46e78acf658
Avvys <bartnicka.g@gmail.com>
2017-08-22 00:48:07 +0200

    First aid kits

--- Assets/Scripts/Player/PlayerHealthRescue.cs
+++ Assets/Scripts/Player/PlayerHealthRescue.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class PlayerHealthRescue : MonoBehaviour
 {
-	public int amount = 10;
+	public int amount = -20;
 
 	public AudioClip rescueClip;
 
 	Animator anim;
 	GameObject player;
 	PlayerHealth playerHealth;
-	bool playerInRange;
+	bool taken;
 	float timer;
 	UnityEngine.AI.NavMeshAgent nav;
 
@@ -20,7 +20,7 @@ public class PlayerHealthRescue : MonoBehaviour
 	{
 		player = GameObject.FindGameObjectWithTag ("Player");
 		playerHealth = player.GetComponent<PlayerHealth>();
-		anim = GetComponent <Animator> ();
+//		anim = GetComponent <Animator> ();
 //		nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
 
 		audio = GetComponent <AudioSource> ();
@@ -30,36 +30,76 @@ public class PlayerHealthRescue : MonoBehaviour
 
 	void applyRescue() { 
 		playerHealth.rescue (amount);
-		playerInRange = true;
-//		nav.enabled = false;
+		nav.enabled = false;
 
 
 		audio.clip = rescueClip;
 		audio.Play ();
 
 		GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+
+	
+		taken = true;
+
+
+//		StartCoroutine(MakeDisappear());
 	}
 
 	void OnCollisionEnter(Collision collision)
 	{
-		applyRescue ();
-		Debug.Log ("collision enter");
+//		applyRescue ();
 	}
 
 
 
-	void Update ()
-	{
-		playerHealth = player.GetComponent<PlayerHealth>();
-	
-	}
+//	void Update ()
+//	{
+//		playerHealth = player.GetComponent<PlayerHealth>();
+
+//	}
 
 
 
 	void OnTriggerEnter (Collider other)
 	{
+
+		playerHealth = player.GetComponent<PlayerHealth>();
 		applyRescue ();
-		Debug.Log ("trigger enter");
+//		Destroy(GetComponent<BoxCollider>());
+//		Destroy (other.gameObject);
+		DestroyImmediate (gameObject);
 	}
 
+
+	IEnumerator MakeDisappear()
+	{
+		if (taken)
+		{
+			// Find and disable the Nav Mesh Agent.
+			GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+
+			// Find the rigidbody component and make it kinematic (since we use Translate to sink the enemy).
+
+					BoxCollider collider = gameObject.GetComponent<BoxCollider>();
+					collider.enabled = false;
+//			CapsuleCollider capsuleCollider = gameObject.GetComponent<CapsuleCollider>();
+//			capsuleCollider.enabled = false;
+
+			// Increase the score by the enemy's score value.
+//			ScoreManager.score += scoreValue;
+
+			yield return new WaitForSeconds(1); //this will wait 5 seconds  
+
+			// The enemy should no sink.
+//			isSinking = true;
+
+//			yield return new WaitForSeconds(2); //this will wait 5 seconds  
+			Destroy(gameObject);
+		}
+	}
+
+
+	void Update() { 
+		StartCoroutine (MakeDisappear());
+	}
 }
